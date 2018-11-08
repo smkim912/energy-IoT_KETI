00e447be5127648fa733b7ec2c8c727c225d63ac
smkim912 <zinjja2kw@gmail.com>
2018-11-08 14:47:53 +0900

    normal lstm

--- onp_dl/keras_lstm.py
+++ onp_dl/keras_lstm.py
@@ -18,19 +18,21 @@ np.set_printoptions(threshold=np.nan, formatter=dict(float=lambda x: "%.3f" % x)
 log = open('log.keras_lstm', 'w')
 
 
-class LossHistory(keras.callbacks.Callback):
+class CustomHistory(keras.callbacks.Callback):
 	def init(self):
-		self.losses=[]
+		self.train_loss=[]
+		self.val_loss=[]    
 
 	def on_epoch_end(self, batch, logs={}):
-		self.losses.append(logs.get('loss'))
+		self.train_loss.append(logs.get('loss'))
+		self.val_loss.append(logs.get('val_loss'))
 
 
 def create_dataset(dataset, look_back, features):
 	dataX, dataY = [], []
 	for i in range(len(dataset)-look_back):
-		dataX.append(dataset[i:(i + look_back), :features])
-		dataY.append(dataset[i + look_back, features])	
+		dataX.append(dataset[i:(i + look_back)])
+		dataY.append(dataset[i + look_back, features-1])	
 	return np.array(dataX), np.array(dataY)
 		
 
@@ -58,14 +60,14 @@ train, test = nptf[0:train_size, :], nptf[train_size:len(nptf), :]
 #print(len(train), len(test))
 	 
 # create dataset for learning
-look_back = 30
-features = 4
+look_back = 300
+features = 5
 trainX, trainY = create_dataset(train, look_back, features)
 testX, testY = create_dataset(test, look_back, features)
 
 # reshape input to be [samples, time steps, features]
 trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], features))
-testX = np.reshape(testX, (testX.shape[0], trainX.shape[1], features))
+testX = np.reshape(testX, (testX.shape[0], testX.shape[1], features))
 
 #print(trainY)
 #print('-------------------')
@@ -82,69 +84,70 @@ testX = np.reshape(testX, (testX.shape[0], trainX.shape[1], features))
 
 # simple lstm network learning
 model = Sequential()
-model.add(LSTM(128, batch_input_shape=(1, look_back, features), stateful=True))
-#model.add(Dense(one_hot_vec_size, activation='softmax'))
-#model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+'''
+for i in range(2):
+	model.add(LSTM(32, batch_input_shape=(1, look_back, features), stateful=True, return_sequences=True))
+	model.add(Dropout(0.3))
+model.add(LSTM(32, batch_input_shape=(1, look_back, features), stateful=True))
 model.add(Dropout(0.3))
 model.add(Dense(1))
 model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+''' 
+model.add(LSTM(128, input_shape=(None, features)))
+model.add(Dropout(0.4))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
 
-history = LossHistory()
-history.init()
+#history = CustomHistory()
+#history.init()
 
-num_epochs = 2
+'''
+num_epochs = 10
 for epoch_idx in range(num_epochs):
 	print('epochs : ' + str(epoch_idx))
 	log.write('epochs : ' + str(epoch_idx) + '\n')
-	model.fit(trainX, trainY, epochs=1, batch_size=features, verbose=2, shuffle=False, callbacks=[history], validation_data=(testX, testY))
+	model.fit(trainX, trainY, epochs=1, batch_size=2, verbose=2, shuffle=False, callbacks=[history], validation_data=(testX, testY))
 	model.reset_states()
+'''
+hist = model.fit(trainX, trainY, epochs=10, batch_size=128, validation_data=(testX, testY))
+model.reset_states()
 
-#%matplotlib inline
-import matplotlib
-matplotlib.use('TkAgg')
+%matplotlib inline
+#import matplotlib
+#matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
-plt.plot(history.losses)
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.ylim(0.0, 0.15)
 plt.ylabel('loss')
 plt.xlabel('epoch')
-plt.legend(['train'], loc='upper left')
+plt.legend(['train', 'val'], loc='upper left')
 plt.show()
-
-train_scores = model.evaluate(trainX, trainY, batch_size=features)
+'''
+train_scores = model.evaluate(trainX, trainY, batch_size=32)
 print("(train)%s: %.3f%%" %(model.metrics_names[0], train_scores[0]*100))
 log.write("(train)%s: %.3f%%\n" %(model.metrics_names[0], train_scores[0]*100))
 print("(train)%s: %.3f%%" %(model.metrics_names[1], train_scores[1]*100))
 log.write("(train)%s: %.3f%%\n" %(model.metrics_names[1], train_scores[1]*100))
 model.reset_states()
-test_scores = model.evaluate(testX, testY, batch_size=features)
+test_scores = model.evaluate(testX, testY, batch_size=32)
 print("(test)%s: %.3f%%" %(model.metrics_names[0], test_scores[0]*100))
 log.write("(test)%s: %.3f%%\n" %(model.metrics_names[0], test_scores[0]*100))
 print("(test)%s: %.3f%%" %(model.metrics_names[1], test_scores[1]*100))
 log.write("(test)%s: %.3f%%\n" %(model.metrics_names[1], test_scores[1]*100))
 model.reset_states()
+'''
+look_ahead = 300
 
-look_ahead = 20
-
-xhat = testX[0]
+#xhat = testX[0]
 predictions = np.zeros((look_ahead, 1))
 for i in range(look_ahead):
-    prediction = model.predict(np.array([xhat]), batch_size=features)
+    prediction = model.predict(np.array([testX[i]]), batch_size=128)
     predictions[i] = prediction
-    xhat = np.vstack([xhat[1:],prediction])
+    #xhat = np.vstack([xhat[1:],prediction])
 
 plt.figure(figsize=(12,5))
 plt.plot(np.arange(look_ahead),predictions,'r',label="prediction")
 plt.plot(np.arange(look_ahead),testY[:look_ahead],label="test function")
 plt.legend()
 plt.show()
-'''
-score = math.sqrt(mean_squared_error(testY, predictions))
-print('Score: %.4f RMSE' % score)
-
-for i in range(pred_count):
-	idx = np.argmax(pred_out[i])
-	seq_out.append(idx)
-
-print(str(seq_out))
-log.write(str(seq_out))
-'''
-model.reset_states()
